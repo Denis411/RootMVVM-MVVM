8addc1db444be944cf16b1fa435984ec0dee3add
Denis <theveryengineer@gmail.com>
2025-08-14 12:18:07 +0700

    Add basic views

--- ProfileViewSample/ContentView.swift
+++ ProfileViewSample/ContentView.swift
@@ -7,15 +7,58 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct BasicView: View {
+    private var isPresented: Bool
+    private let text: String
+    
+    init(
+        isPresented: Bool,
+        text: String
+    ) {
+        self.isPresented = isPresented
+        self.text = text
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if isPresented {
+            HStack {
+                Text(text)
+                    .padding(.vertical)
+                Spacer()
+            }
+            .background(Color.green)
+        } else {
+            EmptyView()
         }
-        .padding()
+    }
+}
+
+extension BasicView: Equatable {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.isPresented == rhs.isPresented
+    }
+}
+
+struct ContentView: View {
+    @State var isPresented = true
+    
+    var body: some View {
+        LazyVStack(spacing: 0) {
+            BasicView(isPresented: true, text: "First")
+            BasicView(isPresented: isPresented, text: "Second")
+            BasicView(isPresented: true, text: "Third")
+            
+            Button {
+                isPresented.toggle()
+            } label: {
+                Text("Hide the second")
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .background(Color.blue)
+                    .padding(.top, 50)
+            }
+
+        }
     }
 }
 
