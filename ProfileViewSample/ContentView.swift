4fb3e9557c2a426d6b23b92919e57b5919b8e837
Denis <theveryengineer@gmail.com>
2025-08-14 14:49:32 +0700

    [GPT] Add boilerplate routine

--- ProfileViewSample/ContentView.swift
+++ ProfileViewSample/ContentView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 class ProfileRootVM: ObservableObject {
-    func foo() {
-        print("Foo")
+    // MARK: - Actions
+    func didTapFavoriteProducts() {
+        print("Favorite products tapped")
+    }
+    
+    func didTapLoyaltyProgramInfo() {
+        print("Loyalty program info tapped")
+    }
+    
+    func didTapGeolocation() {
+        print("Geolocation tapped")
+    }
+    
+    func didTapHelp() {
+        print("Help tapped")
+    }
+    
+    func didTapLegalInfo() {
+        print("Legal info tapped")
+    }
+    
+    func didTapSendResume() {
+        print("Send resume tapped")
     }
 }
 
@@ -23,36 +44,36 @@ struct ContentView: View {
                     leftImage: Image(systemName: "cart"),
                     title: "Покупки"
                 ) {
-                    ChevronButton(text: "Избранные товары", action: vm.foo)
+                    ChevronButton(text: "Избранные товары", action: vm.didTapFavoriteProducts)
                 }
                 
                 ChevronView(
                     leftImage: Image(systemName: "creditcard.and.123"),
                     title: "Программа лояльности"
                 ) {
-                    ChevronButton(text: "О программе лояльности", action: vm.foo)
+                    ChevronButton(text: "О программе лояльности", action: vm.didTapLoyaltyProgramInfo)
                 }
                 
                 ChevronView(
                     leftImage: Image(systemName: "location.fill"),
                     title: "Адрес"
                 ) {
-                    ChevronButton(text: "Геолокация", action: vm.foo)
+                    ChevronButton(text: "Геолокация", action: vm.didTapGeolocation)
                 }
                 
                 ChevronView(
                     leftImage: Image(systemName: "envelope.fill"),
                     title: "Обратная связь"
                 ) {
-                    ChevronButton(text: "Помощь", action: vm.foo)
-                    ChevronButton(text: "Правовая информация", action: vm.foo)
+                    ChevronButton(text: "Помощь", action: vm.didTapHelp)
+                    ChevronButton(text: "Правовая информация", action: vm.didTapLegalInfo)
                 }
                 
                 ChevronView(
                     leftImage: Image(systemName: "backpack"),
                     title: "Работа у нас"
                 ) {
-                    ChevronButton(text: "Отправить резюме", action: vm.foo)
+                    ChevronButton(text: "Отправить резюме", action: vm.didTapSendResume)
                 }
             }
         }
