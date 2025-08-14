19ffb97a8cbf7b0460e7c4c65b81c9ded1a3f8aa
Denis <theveryengineer@gmail.com>
2025-08-14 19:26:19 +0700

    Add logic for listening

--- ProfileViewSample/Helpers/Repository.swift
+++ ProfileViewSample/Helpers/Repository.swift
@@ -7,10 +7,26 @@
 
 import Foundation
 
+protocol RepositoryListener: AnyObject {
+    func userDidLogIn() async
+    func userDidLogOut() async
+}
+
 protocol RepositoryProtocol {
     var isUserLoggedIn: Bool { get }
     func logInUser() async throws
     func logOutUser() async throws
+    
+    // I simplify everything because it is a prototype
+    // But you will have to inject everything properly
+    // splitting protocols could be a good idea too
+    var userListeners: [Weak<AnyObject>] { get }
+    func addListener(_ listener: RepositoryListener)
+}
+
+protocol LogInListener {
+    func userDidLogIn()
+    func userDidLogOut()
 }
 
 final class Repository: RepositoryProtocol {
@@ -18,10 +34,31 @@ final class Repository: RepositoryProtocol {
     
     func logInUser() async throws {
         isUserLoggedIn = true
+        for listener in userListeners {
+            await (listener as? RepositoryListener)?.userDidLogIn()
+        }
     }
     
     func logOutUser() async throws {
         isUserLoggedIn = false
+        for listener in userListeners {
+            await (listener as? RepositoryListener)?.userDidLogOut()
+        }
     }
     
+    // observation
+    private(set) var userListeners: [Weak<AnyObject>] = []
+    
+    func addListener(_ listener: RepositoryListener) {
+        let weakReference = Weak(value: listener as AnyObject)
+        userListeners.append(weakReference)
+    }
+}
+
+class Weak<T: AnyObject> {
+    weak var value: T?
+
+    init(value: T) {
+        self.value = value
+    }
 }
