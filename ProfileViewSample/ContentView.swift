00b5dba772ebbfea5ecbec5ca6c0d7cf0504aad2
Denis <theveryengineer@gmail.com>
2025-08-14 12:08:44 +0700

    Initial Commit

---
+++ ProfileViewSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProfileViewSample
+//
+//  Created by FIX PRICE on 14/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProfileViewSample/ProfileViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewSampleApp.swift
+//  ProfileViewSample
+//
+//  Created by FIX PRICE on 14/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ProfileViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
